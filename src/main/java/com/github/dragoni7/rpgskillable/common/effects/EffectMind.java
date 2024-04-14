a086032721ee83785be1d2acc5e3c459f394d174
Shadow's Peak <99706791+Shadows-Peak@users.noreply.github.com>
2024-04-14 00:28:37 -0700

    Update EffectMind.java

--- src/main/java/com/github/dragoni7/rpgskillable/common/effects/EffectMind.java
+++ src/main/java/com/github/dragoni7/rpgskillable/common/effects/EffectMind.java
@@ -2,8 +2,7 @@ package com.github.dragoni7.rpgskillable.common.effects;
 
 import java.util.UUID;
 
-import com.hollingsworth.arsnouveau.ArsNouveau;
-import com.hollingsworth.arsnouveau.api.perk.PerkAttributes;
+import io.redspace.ironsspellbooks.api.registry.AttributeRegistry;
 
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
@@ -13,11 +12,13 @@ import net.minecraftforge.fml.ModList;
 public class EffectMind extends MobEffect {
 	
 	private static final UUID MAX_MANA_BONUS = UUID.fromString("4e9c93c1-162e-401b-aace-aa4109fd0a5f");
+	private static final UUID CAST_TIME_REDUCTION_BONUS = UUID.formString("9ca8f045-fc7a-43ae-a4a0-8fc9ac8ceba7");
 
 	protected EffectMind() {
 		super(MobEffectCategory.BENEFICIAL, 0X373f8c);
-		if (ModList.get().isLoaded(ArsNouveau.MODID)) {
-			this.addAttributeModifier(PerkAttributes.MAX_MANA_BONUS.get(), MAX_MANA_BONUS.toString(), 0.2f, AttributeModifier.Operation.MULTIPLY_TOTAL);
+		if (ModList.get().isLoaded("irons_spellbooks")) {
+			this.addAttributeModifier(AttributeRegistry.MAX_MANA.get(), MAX_MANA_BONUS.toString(), 0.2f, AttributeModifier.Operation.MULTIPLY_TOTAL);
+			this.addAttributeModifier(AttributeRegistry.CAST_TIME_REDUCTION.get(), CAST_TIME_REDUCTION_BONUS.toString(), -0.2f, AttributeModifier.Operation.MULTIPLY_BASE);
 		}
 	}
 
