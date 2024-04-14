f7e2641cd0043a7361d37dae2a6229984c909528
Shadow's Peak <99706791+Shadows-Peak@users.noreply.github.com>
2024-04-14 00:42:36 -0700

    Update EffectMind.java

--- src/main/java/com/github/dragoni7/rpgskillable/common/effects/EffectMind.java
+++ src/main/java/com/github/dragoni7/rpgskillable/common/effects/EffectMind.java
@@ -12,8 +12,8 @@ import net.minecraftforge.fml.ModList;
 public class EffectMind extends MobEffect {
 	
 	private static final UUID MAX_MANA_BONUS = UUID.fromString("4e9c93c1-162e-401b-aace-aa4109fd0a5f");
-	private static final UUID CAST_TIME_REDUCTION_BONUS = UUID.formString("9ca8f045-fc7a-43ae-a4a0-8fc9ac8ceba7");
-	private static final UUID COOLDOWN_REDUCTION_BONUS = UUID.formString("87495934-7df6-451f-a5bf-589b008156a9");
+	private static final UUID CAST_TIME_REDUCTION_BONUS = UUID.fromString("9ca8f045-fc7a-43ae-a4a0-8fc9ac8ceba7");
+	private static final UUID COOLDOWN_REDUCTION_BONUS = UUID.fromString("87495934-7df6-451f-a5bf-589b008156a9");
 
 	protected EffectMind() {
 		super(MobEffectCategory.BENEFICIAL, 0X373f8c);
